24ac32c8fd822625f0f698e4ca35154c67ae6664
Dmitry Gritsay <unseductable@gmail.com>
2015-10-30 23:51:24 +0200

    Create unsafe_bindings::class module

---
+++ src/unsafe_bindings/class.rs
@@ -0,0 +1,17 @@
+use types;
+
+#[link(name = "ruby")]
+extern "C" {
+    pub fn rb_define_class(name: *const types::c_char, superclass: types::rb_value) -> types::rb_value;
+    pub fn rb_define_module(name: *const types::c_char) -> types::rb_value;
+
+    pub fn rb_define_method(klass: types::rb_value,
+                            name: *const types::c_char,
+                            callback: extern fn(types::rb_value) -> types::rb_value,
+                            argc: types::c_int);
+
+    pub fn rb_define_singleton_method(klass: types::rb_value,
+                                      name: *const types::c_char,
+                                      callback: extern fn(types::rb_value) -> types::rb_value,
+                                      argc: types::c_int);
+}
