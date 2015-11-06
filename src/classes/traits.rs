a9ebfdb0f71f9d9a5a8ef9beba08de3ca6695253
Dmitry Gritsay <unseductable@gmail.com>
2015-11-06 22:12:50 +0200

    Implement conversions for RawObject
     - Use `as_class` to convert to `Class`
     - Use `as_array` to convert to `Array`

--- src/classes/traits.rs
+++ src/classes/traits.rs
@@ -1,7 +1,17 @@
 use types;
 
+use super::{class, array};
+
 pub trait RawObject {
     fn from_value(value: types::rb_value) -> Self;
 
     fn value(&self) -> types::rb_value;
+
+    fn as_class(&self) -> class::Class {
+        class::Class::from_value(self.value())
+    }
+
+    fn as_array(&self) -> array::Array {
+        array::Array::from_value(self.value())
+    }
 }
