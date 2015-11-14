668f32fd9ac8d290a0d3d916f53cf7f5eb2b4753
Dmitry Gritsay <unseductable@gmail.com>
2015-11-14 07:59:01 +0200

    Rename cstr_as_str to cstr_as_string

--- src/util.rs
+++ src/util.rs
@@ -6,7 +6,7 @@ pub fn str_as_ptr(str: &str) -> *const types::c_char {
     CString::new(str).unwrap().as_ptr()
 }
 
-pub fn cstr_as_str(str: *const types::c_char) -> String {
+pub fn cstr_as_string(str: *const types::c_char) -> String {
     unsafe {
         CStr::from_ptr(str).to_string_lossy().into_owned()
     }
