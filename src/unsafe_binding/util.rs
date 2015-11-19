c33fcfa8b9bba2329db72e1aaa95725de0c31b67
Dmitry Gritsay <unseductable@gmail.com>
2015-11-19 08:43:17 +0200

    Bind rb_funcall2 instead of rb_funcall

--- src/unsafe_binding/util.rs
+++ src/unsafe_binding/util.rs
@@ -4,8 +4,8 @@ use types;
 extern "C" {
     pub fn rb_intern(name: *const types::c_char) -> types::rb_value;
     pub fn rb_const_get(klass: types::rb_value, id: types::rb_value) -> types::rb_value;
-    pub fn rb_funcall(receiver: types::rb_value,
-                      method: types::rb_value,
-                      argc: types::c_int,
-                      ...) -> types::rb_value;
+    pub fn rb_funcall2(receiver: types::rb_value,
+                       method: types::rb_value,
+                       argc: types::c_int,
+                       argv: *const types::rb_value) -> types::rb_value;
 }
