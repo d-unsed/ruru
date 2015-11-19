479f4c809fb61bd720ea4a43e1268915152856fe
Dmitry Gritsay <unseductable@gmail.com>
2015-11-19 07:55:16 +0200

    Implement RawObject as_string()

--- src/class/traits.rs
+++ src/class/traits.rs
@@ -1,17 +1,21 @@
 use types;
 
-use super::{class, array};
+use super::{array, class, string};
 
 pub trait RawObject {
     fn from_value(value: types::rb_value) -> Self;
 
     fn value(&self) -> types::rb_value;
 
+    fn as_array(&self) -> array::Array {
+        array::Array::from_value(self.value())
+    }
+
     fn as_class(&self) -> class::Class {
         class::Class::from_value(self.value())
     }
 
-    fn as_array(&self) -> array::Array {
-        array::Array::from_value(self.value())
+    fn as_string(&self) -> string::RString {
+        string::RString::from_value(self.value())
     }
 }
