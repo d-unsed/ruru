8048c2e077b509a3223a311aca467fee8cae4ee7
Dmitry Gritsay <unseductable@gmail.com>
2015-11-27 07:58:01 +0200

    Create binding::call_method

--- src/binding/util.rs
+++ src/binding/util.rs
@@ -6,3 +6,15 @@ pub fn internal_id(string: &str) -> types::rb_id {
         util::rb_intern(::util::str_as_ptr(string))
     }
 }
+
+pub fn call_method(receiver: types::rb_value,
+                   method: &str,
+                   arguments: Vec<types::rb_value>) -> types::rb_value {
+    let method_id = internal_id(method);
+    let argc = arguments.len() as types::c_int;
+    let argv = arguments.as_ptr();
+
+    unsafe {
+        util::rb_funcall2(receiver, method_id, argc, argv)
+    }
+}
