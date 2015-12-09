ef254a9278b827f7ab0d406af91e0aa94a8e5412
Dmitry Gritsay <unseductable@gmail.com>
2015-12-09 19:54:22 +0200

    Create binding for rb_id2name

--- src/binding/util.rs
+++ src/binding/util.rs
@@ -19,6 +19,14 @@ pub fn id_to_sym(id: types::rb_id) -> types::rb_value {
     }
 }
 
+pub fn id_to_name(id: types::rb_id) -> String {
+    unsafe {
+        let str = util::rb_id2name(id);
+
+        ::util::cstr_as_string(str)
+    }
+}
+
 pub fn sym_to_id(sym: types::rb_value) -> types::rb_id {
     unsafe {
         util::rb_sym2id(sym)
--- src/unsafe_binding/util.rs
+++ src/unsafe_binding/util.rs
@@ -6,6 +6,7 @@ extern "C" {
     pub fn rb_num2int(num: types::rb_value) -> types::rb_signed_value;
 
     pub fn rb_id2sym(id: types::rb_id) -> types::rb_value;
+    pub fn rb_id2name(id: types::rb_id) -> *const types::c_char;
     pub fn rb_sym2id(id: types::rb_value) -> types::rb_id;
 
     pub fn rb_intern(name: *const types::c_char) -> types::rb_id;
