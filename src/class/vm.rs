b818f1ece517a11e118c5771b183d232882e8fea
Dmitry Gritsay <unseductable@gmail.com>
2015-12-11 20:47:48 +0200

    Create VM parse_arguments()

--- src/class/vm.rs
+++ src/class/vm.rs
@@ -1,9 +1,21 @@
+use std::slice;
+
 use binding::vm;
+use class::object;
+use types;
 
 pub struct VM;
 
 impl VM {
     pub fn init() {
         vm::init();
+    }
+
+    pub fn parse_arguments(argc: types::argc, arguments: *const types::rb_value) -> Vec<object::Object> {
+        let arguments = unsafe {
+            slice::from_raw_parts(arguments, argc as usize).to_vec()
+        };
+
+        arguments.iter().map(|value| object::Object::from(*value)).collect()
     }
 }
