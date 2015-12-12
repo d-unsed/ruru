47a8994cf7906fad27dffa1c96cac67eea5fce97
Dmitry Gritsay <unseductable@gmail.com>
2015-12-12 17:30:29 +0200

    Create RawObject::class()
     - Return a `Class` structure of current object

--- src/class/traits.rs
+++ src/class/traits.rs
@@ -1,5 +1,6 @@
 use std::convert::From;
 
+use binding;
 use binding::util;
 use types;
 
@@ -7,6 +8,12 @@ use super::{array, class, hash, object, string};
 
 pub trait RawObject : From<types::rb_value> {
     fn value(&self) -> types::rb_value;
+
+    fn class(&self) -> class::Class {
+        let class = binding::class::object_class(self.value());
+
+        class::Class::from(class)
+    }
 
     fn as_array(&self) -> array::Array {
         array::Array::from(self.value())
