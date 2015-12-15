cee25223d0d5857b2eab96e48d18fe851bb8db93
Dmitry Gritsay <unseductable@gmail.com>
2015-12-15 20:08:25 +0200

    Move conversion methods from RawObject to Object

--- src/class/object.rs
+++ src/class/object.rs
@@ -2,10 +2,33 @@ use std::convert::From;
 
 use types;
 
+use super::{array, class, fixnum, hash, string};
 use super::traits::RawObject;
 
 pub struct Object {
     value: types::rb_value
+}
+
+impl Object {
+    pub fn as_array(&self) -> array::Array {
+        array::Array::from(self.value())
+    }
+
+    pub fn as_class(&self) -> class::Class {
+        class::Class::from(self.value())
+    }
+
+    pub fn as_fixnum(&self) -> fixnum::Fixnum {
+        fixnum::Fixnum::from(self.value())
+    }
+
+    pub fn as_hash(&self) -> hash::Hash {
+        hash::Hash::from(self.value())
+    }
+
+    pub fn as_string(&self) -> string::RString {
+        string::RString::from(self.value())
+    }
 }
 
 impl From<types::rb_value> for Object {
--- src/class/traits.rs
+++ src/class/traits.rs
@@ -4,7 +4,7 @@ use binding;
 use binding::util;
 use types;
 
-use super::{array, class, fixnum, hash, object, string};
+use super::{class, object};
 
 pub trait RawObject : From<types::rb_value> {
     fn value(&self) -> types::rb_value;
@@ -13,30 +13,6 @@ pub trait RawObject : From<types::rb_value> {
         let class = binding::class::object_class(self.value());
 
         class::Class::from(class)
-    }
-
-    fn as_array(&self) -> array::Array {
-        array::Array::from(self.value())
-    }
-
-    fn as_class(&self) -> class::Class {
-        class::Class::from(self.value())
-    }
-
-    fn as_fixnum(&self) -> fixnum::Fixnum {
-        fixnum::Fixnum::from(self.value())
-    }
-
-    fn as_hash(&self) -> hash::Hash {
-        hash::Hash::from(self.value())
-    }
-
-    fn as_object(&self) -> object::Object {
-        object::Object::from(self.value())
-    }
-
-    fn as_string(&self) -> string::RString {
-        string::RString::from(self.value())
     }
 
     fn send(&self, method: &str, arguments: Vec<object::Object>) -> object::Object {
