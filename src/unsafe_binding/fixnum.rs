fc1890d7cbb473fc1f3be0012e3abbd97ed5b4a6
Dmitry Gritsay <unseductable@gmail.com>
2015-12-16 18:48:40 +0200

    Update rb_num2int to use c_long

--- src/unsafe_binding/fixnum.rs
+++ src/unsafe_binding/fixnum.rs
@@ -3,5 +3,5 @@ use types;
 #[link(name = "ruby")]
 extern "C" {
     pub fn rb_int2inum(num: types::rb_signed_value) -> types::rb_value;
-    pub fn rb_num2int(num: types::rb_value) -> types::rb_signed_value;
+    pub fn rb_num2int(num: types::rb_value) -> types::c_long;
 }
