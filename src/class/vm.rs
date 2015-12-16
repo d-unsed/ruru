0cb66f7aa17604b9ad655da0bb9e251fbcd82353
Dmitry Gritsay <unseductable@gmail.com>
2015-12-16 19:10:53 +0200

    Create VM::create_arguments()

--- src/class/vm.rs
+++ src/class/vm.rs
@@ -4,6 +4,8 @@ use binding::vm;
 use class::object;
 use types;
 
+use class::traits::RawObject;
+
 pub struct VM;
 
 impl VM {
@@ -19,7 +21,19 @@ impl VM {
         arguments.iter().map(|value| object::Object::from(*value)).collect()
     }
 
+    pub fn create_arguments(arguments: Vec<object::Object>) -> (types::c_int, *const types::rb_value) {
+        (arguments.len() as types::c_int, Self::arguments_as_ptr(arguments))
+    }
+
     pub fn parse_itself(itself: types::rb_value) -> object::Object {
         object::Object::from(itself)
+    }
+
+    fn arguments_as_ptr(arguments: Vec<object::Object>) -> *const types::rb_value {
+        arguments
+            .iter()
+            .map(|object| object.value())
+            .collect::<Vec<types::rb_value>>()
+            .as_ptr()
     }
 }
