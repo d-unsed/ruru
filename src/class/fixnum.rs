32c2fed66bf795999f4cd8fe32780e092160487c
Dmitry Gritsay <unseductable@gmail.com>
2016-01-23 19:01:00 +0200

    Create documentation for Fixnum

--- src/class/fixnum.rs
+++ src/class/fixnum.rs
@@ -5,18 +5,43 @@ use binding::fixnum::{int_to_num, num_to_int};
 
 use super::traits::Object;
 
+/// `Fixnum`
 #[derive(Debug, PartialEq)]
 pub struct Fixnum {
     value: Value
 }
 
 impl Fixnum {
+    /// Creates a new `Fixnum`
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use ruru::{Fixnum, VM};
+    /// # VM::init();
+    ///
+    /// let fixnum = Fixnum::new(1);
+    ///
+    /// assert_eq!(fixnum.to_i64(), 1);
+    /// ```
     pub fn new(num: i64) -> Self {
         Fixnum {
             value: int_to_num(num)
         }
     }
 
+    /// Retrieves an `i64` value from `Fixnum`
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use ruru::{Fixnum, VM};
+    /// # VM::init();
+    ///
+    /// let fixnum = Fixnum::new(1);
+    ///
+    /// assert_eq!(fixnum.to_i64(), 1);
+    /// ```
     pub fn to_i64(&self) -> i64 {
         num_to_int(self.value)
     }
