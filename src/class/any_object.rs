9177c34a380f4dd141aa0df512fdcacc10524d4c
Dmitry Gritsay <unseductable@gmail.com>
2016-01-25 20:54:45 +0200

    Create documentation for AnyObject

--- src/class/any_object.rs
+++ src/class/any_object.rs
@@ -10,36 +10,110 @@ use super::symbol::Symbol;
 
 use super::traits::Object;
 
+/// Representation of any Ruby object while its type is unknown
+///
+/// As Ruby is a dynamically typed language, at some points Ruru does not know the exact Ruby type
+/// of the object. It happens in the following cases:
+///
+/// - Retrieving an object from array
+///
+/// - Retrieving an object from hash
+///
+/// - Receiving arguments to callback
+///
+/// - Initializing new instance of a class which is not present in Ruru
+///
+/// - and some other places you can find in Ruru documentation
+///
+/// In these cases you should cast `AnyObject` to the type to which the object belongs
+/// using functions below.
+///
+/// # Examples
+///
+/// ## Retrieving an object from `Array`
+///
+/// ```
+/// use ruru::{Array, Fixnum, VM};
+/// # VM::init();
+///
+/// let array = Array::new().push(Fixnum::new(1));
+/// let value = array.at(0).as_fixnum(); // `Array::at()` returns `AnyObject`
+///
+/// assert_eq!(value.to_i64(), 1);
+/// ```
+///
+/// ## Retrieving an object from `Hash`
+///
+/// ```no_run
+/// use ruru::{Fixnum, Hash, Symbol, VM};
+/// # VM::init();
+///
+/// let mut hash = Hash::new();
+///
+/// hash.store(Symbol::new("key"), Fixnum::new(1));
+///
+/// // `Hash::at()` returns `AnyObject`
+/// let value = hash.at(Symbol::new("key")).as_fixnum();
+///
+/// assert_eq!(value, Fixnum::new(1));
+/// ```
+///
+/// ## Receiving arguments from Ruby to Ruru callback
+///
+/// ```no_run
+/// use ruru::types::Argc;
+/// use ruru::{AnyObject, Boolean, Class, RString, VM};
+///
+/// #[no_mangle]
+/// pub extern fn string_eq(argc: Argc, argv: *const AnyObject, itself: RString) -> Boolean {
+///     // `VM::parse_arguments()` returns `Vec<AnyObject>`
+///     let argv = VM::parse_arguments(argc, argv);
+///     let other_string = argv[0].as_string();
+///
+///     Boolean::new(itself.to_string() == other_string.to_string())
+/// }
+///
+/// fn main() {
+///     Class::from_existing("String").define_method("==", string_eq);
+/// }
+/// ```
 #[derive(Clone)]
 pub struct AnyObject {
     value: Value
 }
 
 impl AnyObject {
+    /// Casts to `Array`.
     pub fn as_array(&self) -> Array {
         Array::from(self.value)
     }
 
+    /// Casts to `Boolean`.
     pub fn as_boolean(&self) -> Boolean {
         Boolean::from(self.value)
     }
 
+    /// Casts to `Class`.
     pub fn as_class(&self) -> Class {
         Class::from(self.value)
     }
 
+    /// Casts to `Fixnum`.
     pub fn as_fixnum(&self) -> Fixnum {
         Fixnum::from(self.value)
     }
 
+    /// Casts to `Hash`.
     pub fn as_hash(&self) -> Hash {
         Hash::from(self.value)
     }
 
+    /// Casts to `String`.
     pub fn as_string(&self) -> RString {
         RString::from(self.value)
     }
 
+    /// Casts to `Symbol`.
     pub fn as_symbol(&self) -> Symbol {
         Symbol::from(self.value)
     }
