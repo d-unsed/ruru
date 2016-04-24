9d91edb10f50b910af50856c65ac8929e411558d
Steve Klabnik <steve@steveklabnik.com>
2016-04-24 03:56:27 -0400

    Fix a use-after-free.
    CString owns the memory for the string it creates, and so when it goes
    out of scope, it will drop the memory. This means that the old usage of
    it hands out a dangling pointer, which led to use-after-free. This code
    isn't super clean, but it does the job for now.

--- src/binding/string.rs
+++ src/binding/string.rs
@@ -1,10 +1,13 @@
 use types::Value;
 use unsafe_binding::string::{rb_str_new_cstr, rb_string_value_cstr};
-use util::{cstr_as_string, str_as_ptr};
+use util::cstr_as_string;
+use std::ffi::CString;
 
 pub fn new(string: &str) -> Value {
+    let string = CString::new(string).unwrap();
+
     unsafe {
-        rb_str_new_cstr(str_as_ptr(string))
+        rb_str_new_cstr(string.as_ptr())
     }
 }
 
