40db4ee56a1159c20e18556cfc3d7720926a079a
Mark Lee <rust.ruby@lazymalevolence.com>
2016-05-12 17:55:57 -0700

    Use ruby-sys 0.2.1
    This means re-adding build.rs.

---
+++ build.rs
@@ -0,0 +1,14 @@
+use std::process::Command;
+
+fn main() {
+    let ruby = Command::new("ruby")
+                   .arg("-e")
+                   .arg("puts File.join(File.dirname(File.dirname(RbConfig.ruby)), 'lib')")
+                   .output()
+                   .unwrap_or_else(|e| panic!("ruby not found: {}", e));
+
+    let ruby_libdir = String::from_utf8_lossy(&ruby.stdout);
+
+    println!("cargo:rustc-link-search={}", ruby_libdir.trim());
+    println!("cargo:rustc-link-lib=dylib=ruby");
+}
