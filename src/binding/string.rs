8a582c7e6d4a06715b4b25859038db0bcf00e4b3
Dmitry Gritsay <unseductable@gmail.com>
2016-07-04 23:18:37 +0300

    Add RString::to_string_unchecked()
     - Add `RString::to_string_unchecked()` for getting strings
       which contain interiorn null-bytes from Ruby to Rust

--- src/binding/string.rs
+++ src/binding/string.rs
@@ -1,5 +1,7 @@
+use std::slice::from_raw_parts;
+
+use ruby_sys::string::{rb_str_len, rb_str_new_cstr, rb_string_value_cstr, rb_string_value_ptr};
 use types::Value;
-use ruby_sys::string::{rb_str_new_cstr, rb_string_value_cstr};
 use util::{cstr_as_string, str_to_cstring};
 
 pub fn new(string: &str) -> Value {
@@ -11,5 +13,16 @@ pub fn from_value(value: Value) -> String {
         let str = rb_string_value_cstr(&value);
 
         cstr_as_string(str)
+    }
+}
+
+pub fn from_value_unchecked(value: Value) -> String {
+    unsafe {
+        let str = rb_string_value_ptr(&value) as *const u8;
+        let len = rb_str_len(value) as usize;
+
+        let vec = from_raw_parts(str, len).to_vec();
+
+        String::from_utf8_unchecked(vec)
     }
 }
--- src/class/string.rs
+++ src/class/string.rs
@@ -1,6 +1,6 @@
 use std::convert::From;
 
-use binding::string::{from_value, new};
+use binding::string::{from_value, from_value_unchecked, new};
 use types::Value;
 
 use super::traits::Object;
@@ -57,6 +57,34 @@ impl RString {
     /// ```
     pub fn to_string(&self) -> String {
         from_value(self.value)
+    }
+
+    /// Retrieves underlying Rust `String` from Ruby `String` object.
+    ///
+    /// Unlike `to_string()` it does not perform any checks for internal null-bytes.
+    ///
+    /// This function may be used to safely get binary data from Ruby.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use ruru::{RString, VM};
+    /// # VM::init();
+    ///
+    /// let string = RString::new("Hello,\0World!");
+    ///
+    /// assert_eq!(string.to_string_unchecked(), "Hello,\0World!".to_string());
+    /// ```
+    ///
+    /// Ruby:
+    ///
+    /// ```ruby
+    /// str = 'Hello,\0World!'
+    ///
+    /// str == 'Hello,\0World!'
+    /// ```
+    pub fn to_string_unchecked(&self) -> String {
+        from_value_unchecked(self.value)
     }
 }
 
