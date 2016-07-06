dd7584ac6c80902eb51c36211334bfd84c9ae042
Dmitry Gritsay <unseductable@gmail.com>
2016-07-06 19:40:18 +0300

    Re-export ruby-sys types
     - Re-export types from `ruby-sys` instead of creating new
       types

--- src/types.rs
+++ src/types.rs
@@ -1,16 +1,6 @@
-use ruby_sys::types;
-
 use class::any_object::AnyObject;
 
-pub use libc::{c_char, c_int, c_long};
-pub use ruby_sys::types::ValueType;
-
-pub type Value = types::Value;
-pub type InternalValue = types::InternalValue;
-pub type SignedValue = types::SignedValue;
-pub type Id = types::Id;
-
-pub type Argc = types::Argc;
+pub use ruby_sys::types::{Argc, c_char, c_int, c_long, CallbackPtr, Id, InternalValue,
+                          SignedValue, Value, ValueType};
 
 pub type Callback<I, O> = extern "C" fn(Argc, *const AnyObject, I) -> O;
-pub type CallbackPtr = types::CallbackPtr;
