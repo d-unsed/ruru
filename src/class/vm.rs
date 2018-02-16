e5d4e051d0be9e4cd50eca3ca3fba72c9732717d
Daniel P. Clark <6ftdan@gmail.com>
2018-02-15 21:25:05 -0500

    Exception message example

--- src/class/vm.rs
+++ src/class/vm.rs
@@ -129,6 +129,32 @@ impl VM {
     /// }
     /// ```
     ///
+    /// `Err` will return an `AnyObject` of the exception class raised.
+    /// 
+    /// 
+    /// ```
+    /// #[macro_use]
+    /// extern crate ruru;
+    ///
+    /// use ruru::{Class, Fixnum, Object, RString, VM};
+    ///
+    /// fn main() {
+    ///     # VM::init();
+    ///
+    ///     let result = VM::eval("raise IndexError, 'flowers'");
+    ///     
+    ///     match result {
+    ///       Err(ao) => {
+    ///         let err = Class::from(ao.value());
+    ///         let message = err.send("message", None);
+    ///         let s = message.try_convert_to::<RString>();
+    ///         assert_eq!(s.ok().unwrap().to_string(), "flowers");
+    ///       },
+    ///       _ => { unreachable!() }
+    ///     }
+    /// }
+    /// ```
+    ///
     /// Be aware when checking for equality amongst types like strings, that even
     /// with the same content in Ruby, they will evaluate to different values in
     /// C/Rust.
